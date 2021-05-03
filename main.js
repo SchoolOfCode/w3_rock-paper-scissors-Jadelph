ad4eb1620062aad73df3c6df28dd9d9f2c0a612a
Jade Phipps <jadelph@outlook.com>
2021-05-04 00:15:58 +0100

    console log working with choice when image clicked

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
-const buttons = document.querySelectorAll("button")
-const computer = document.querySelector("computer")
-const output = docuuemnt.querySelector("output")
+//Set up for use of DOM in the JS File - "reference point"
+const computerScore = 0;
+const playerScore = 0;
+const playerScoreSpan = document.getElementById("player-score");
+const computerScoreSpan = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector("score-board");
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+
+rock.addEventListener('click', function() {
+    console.log("You chose Rock 🤘");
+})
+
+paper.addEventListener('click', function() {
+    console.log("You chose Paper 📰");
+})
+
+scissors.addEventListener('click', function() {
+    console.log("You chose Scissors ✂️");
+})
